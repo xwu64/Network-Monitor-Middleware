e1cdf3d91f7bc2df1678967d7599a23020bac8a5
wxl <xwu64@hawk.iit.edu>
2016-04-12 20:20:48 -0500

    add ARP poisoning test topo

---
+++ ARP-Poisoning.py
@@ -0,0 +1,35 @@
+"""Custom topology example
+One switche plus 3 host :
+
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+
+This topo used to test ARP poisoning
+"""
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        maliciousHost = self.addHost( 'h1',ip='10.0.0.2' )
+        leftHost = self.addHost( 'h2',ip='10.0.0.3' )
+        rightHost = self.addHost( 'h3',ip='10.0.0.4' )
+        rightSwitch = self.addSwitch( 's1')
+
+
+
+        # Add links
+        self.addLink( leftHost,  rightSwitch)
+        self.addLink( rightSwitch, rightHost )
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
